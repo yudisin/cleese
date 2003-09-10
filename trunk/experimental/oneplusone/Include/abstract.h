28dbee1704ff8247a0b64cd04d447bc258bd901b
jtauber <jtauber@48ad7985-ff29-0410-b195-4b37543e66af>
2003-09-10 14:15:29 +0000

    removed unused code

--- trunk/experimental/oneplusone/Include/abstract.h
+++
@@ -1,8 +0,0 @@
-#ifndef Py_ABSTRACTOBJECT_H
-#define Py_ABSTRACTOBJECT_H
-
-PyAPI_FUNC(int) PyNumber_Check(PyObject *o);
-
-PyAPI_FUNC(PyObject *) PyNumber_Add(PyObject *o1, PyObject *o2);
-
-#endif /* !Py_ABSTRACTOBJECT_H */
