02d13be9798061fa0f9b316b742533c1a89ffc7a
jtauber <jtauber@48ad7985-ff29-0410-b195-4b37543e66af>
2003-09-14 11:13:59 +0000

    added printing of malloc info for debugging; changed heap start; added casts to stop warnings; formatted code

--- trunk/experimental/boot2py/mach/malloc.c
+++ trunk/experimental/boot2py/mach/malloc.c
@@ -1,9 +1,17 @@
 #include "Python.h"
 
-static void *curr = 0x50000;
+static void *curr = (void *) 0x00100000;
 
 void *
 malloc(size_t size)
 {
-	return curr += size;
+#ifdef VERBOSE_MALLOC
+	print("malloc ");
+	print_hex(size);
+	print(" @ ");
+	print_hex(curr);
+	print("   ");
+#endif
+	
+	return (void *) (curr += size);
 }
