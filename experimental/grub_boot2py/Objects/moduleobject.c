1e15b9837f22460fefaababec8c0b0920dd317bf
jtauber <jtauber@48ad7985-ff29-0410-b195-4b37543e66af>
2003-09-15 17:55:37 +0000

    filled out PyTypeObject struct for PyModule_Type and displayed error if PyModule_Check fails

--- experimental/grub_boot2py/Objects/moduleobject.c
+++ experimental/grub_boot2py/Objects/moduleobject.c
@@ -36,7 +36,7 @@ PyModule_GetDict(PyObject *m)
 {
 	PyObject *d;
 	if (!PyModule_Check(m)) {
-	  /* ERROR */
+		print(" *** NOT A MODULE *** ");
 		return NULL;
 	}
 	d = ((PyModuleObject *)m) -> md_dict;
@@ -47,6 +47,44 @@ PyModule_GetDict(PyObject *m)
 
 PyTypeObject PyModule_Type = {
 	PyObject_HEAD_INIT(&PyType_Type)
-	/* TO DO */
+	0,					/* ob_size */
+	"module",				/* tp_name */
+	sizeof(PyModuleObject),			/* tp_size */
+	0,					/* tp_itemsize */
+	0, //(destructor)module_dealloc,		/* tp_dealloc */
+	0,					/* tp_print */
+	0,					/* tp_getattr */
+	0,					/* tp_setattr */
+	0,					/* tp_compare */
+	0, //(reprfunc)module_repr,			/* tp_repr */
+	0,					/* tp_as_number */
+	0,					/* tp_as_sequence */
+	0,					/* tp_as_mapping */
+	0,					/* tp_hash */
+	0,					/* tp_call */
+	0,					/* tp_str */
+	0, //PyObject_GenericGetAttr,		/* tp_getattro */
+	0, //PyObject_GenericSetAttr,		/* tp_setattro */
+	0,					/* tp_as_buffer */
+	0, //Py_TPFLAGS_DEFAULT | Py_TPFLAGS_HAVE_GC | Py_TPFLAGS_BASETYPE,		/* tp_flags */
+	0, //module_doc,				/* tp_doc */
+	0, //(traverseproc)module_traverse,		/* tp_traverse */
+	0,					/* tp_clear */
+	0,					/* tp_richcompare */
+	0,					/* tp_weaklistoffset */
+	0,					/* tp_iter */
+	0,					/* tp_iternext */
+	0,					/* tp_methods */
+	0, //module_members,				/* tp_members */
+	0,					/* tp_getset */
+	0,					/* tp_base */
+	0,					/* tp_dict */
+	0,					/* tp_descr_get */
+	0,					/* tp_descr_set */
+	0, //offsetof(PyModuleObject, md_dict),	/* tp_dictoffset */
+	0, //(initproc)module_init,			/* tp_init */
+	0, //PyType_GenericAlloc,			/* tp_alloc */
+	0, //PyType_GenericNew,			/* tp_new */
+	0, //PyObject_GC_Del,		        /* tp_free */
 };
 
