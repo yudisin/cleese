baa4993e59322cc2dc7d6bbdb6c95b7fd8c08afc
jtauber <jtauber@48ad7985-ff29-0410-b195-4b37543e66af>
2003-09-18 16:35:58 +0000

    initial check-in

---
+++ experimental/echo/kernel/main.c
@@ -0,0 +1,21 @@
+#include "Python.h"
+
+extern struct _frozen *frozenModules;
+
+void
+Py_Main()
+{
+	PyImport_FrozenModules = frozenModules;
+	Py_Initialize();
+	PyImport_ImportFrozenModule("__main__");
+	Py_Finalize();
+}
+
+int
+main(void)
+{
+        clrscr();
+        print("Cleese");
+        Py_Main();
+	return 0;
+}
