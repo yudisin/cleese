7774127c7cac4fb887deb51b3bcb1675fdbc3dfc
jtauber <jtauber@48ad7985-ff29-0410-b195-4b37543e66af>
2003-09-19 01:08:06 +0000

    new opcodes in support of echo

--- trunk/experimental/echo/python/Include/opcode.h
+++ trunk/experimental/echo/python/Include/opcode.h
@@ -10,7 +10,16 @@
 
 #define HAVE_ARGUMENT 90
 
-#define LOAD_CONST 100
+#define STORE_NAME     90
+
+#define LOAD_CONST	100	/* Index in const list */
+#define LOAD_NAME	101	/* Index in name list */
+
+#define JUMP_FORWARD	110	/* Number of bytes to skip */
+
+#define SETUP_LOOP	120	/* Target address (absolute) */
+
+#define LOAD_FAST	124	/* Local variable number */
 
 #define HAS_ARG(op) ((op) >= HAVE_ARGUMENT)
 
