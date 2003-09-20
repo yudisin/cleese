76987322bfdbab6e04514c9c3ee5fb0db3f1378d
jtauber <jtauber@48ad7985-ff29-0410-b195-4b37543e66af>
2003-09-20 19:00:40 +0000

    added decl for base string type, resize and format

--- experimental/echo/python/Include/stringobject.h
+++ experimental/echo/python/Include/stringobject.h
@@ -12,6 +12,7 @@ typedef struct {
 #define SSTATE_INTERNED_MORTAL 1
 #define SSTATE_INTERNED_IMMORTAL 2
 
+PyAPI_DATA(PyTypeObject) PyBaseString_Type;
 PyAPI_DATA(PyTypeObject) PyString_Type;
 
 #define PyString_Check(op) PyObject_TypeCheck(op, &PyString_Type)
@@ -21,8 +22,10 @@ PyAPI_FUNC(PyObject *) PyString_FromStringAndSize(const char *, int);
 PyAPI_FUNC(PyObject *) PyString_FromString(const char *);
 
 PyAPI_FUNC(char *) PyString_AsString(PyObject *);
+PyAPI_FUNC(int) _PyString_Resize(PyObject **, int);
 
 PyAPI_FUNC(int) _PyString_Eq(PyObject *, PyObject *);
+PyAPI_FUNC(PyObject *) PyString_Format(PyObject *, PyObject *);
 
 PyAPI_FUNC(void) PyString_InternInPlace(PyObject **);
 PyAPI_FUNC(PyObject *) PyString_InternFromString(const char *);
