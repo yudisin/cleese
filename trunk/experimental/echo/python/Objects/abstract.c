23025f760c14046c4edc05e8c22422c694d7299b
jtauber <jtauber@48ad7985-ff29-0410-b195-4b37543e66af>
2003-09-20 18:48:53 +0000

    new python object support

---
+++ trunk/experimental/echo/python/Objects/abstract.c
@@ -0,0 +1,111 @@
+#include "Python.h"
+
+#define NEW_STYLE_NUMBER(o) PyType_HasFeature((o)->ob_type, \
+				Py_TPFLAGS_CHECKTYPES)
+
+
+
+
+#define NB_SLOT(x) offsetof(PyNumberMethods, x)
+#define NB_BINOP(nb_methods, slot) \
+		(*(binaryfunc*)(& ((char*)nb_methods)[slot]))
+
+
+static PyObject *
+binary_op1(PyObject *v, PyObject *w, const int op_slot)
+{
+	PyObject *x;
+	binaryfunc slotv = NULL;
+	binaryfunc slotw = NULL;
+
+	if (v->ob_type->tp_as_number != NULL && NEW_STYLE_NUMBER(v))
+		slotv = NB_BINOP(v->ob_type->tp_as_number, op_slot);
+	if (w->ob_type != v->ob_type &&
+	    w->ob_type->tp_as_number != NULL && NEW_STYLE_NUMBER(w)) {
+		slotw = NB_BINOP(w->ob_type->tp_as_number, op_slot);
+		if (slotw == slotv)
+			slotw = NULL;
+	}
+	if (slotv) {
+		if (slotw && PyType_IsSubtype(w->ob_type, v->ob_type)) {
+			x = slotw(v, w);
+			if (x != Py_NotImplemented)
+				return x;
+			Py_DECREF(x); /* can't do it */
+			slotw = NULL;
+		}
+		x = slotv(v, w);
+		if (x != Py_NotImplemented)
+			return x;
+		Py_DECREF(x); /* can't do it */
+	}
+	if (slotw) {
+		x = slotw(v, w);
+		if (x != Py_NotImplemented)
+			return x;
+		Py_DECREF(x); /* can't do it */
+	}
+
+	if (!NEW_STYLE_NUMBER(v) || !NEW_STYLE_NUMBER(w)) {
+
+		int err = PyNumber_CoerceEx(&v, &w);
+		if (err < 0) {
+			return NULL;
+		}
+		if (err == 0) {
+			PyNumberMethods *mv = v->ob_type->tp_as_number;
+			if (mv) {
+				binaryfunc slot;
+				slot = NB_BINOP(mv, op_slot);
+				if (slot) {
+					PyObject *x = slot(v, w);
+					Py_DECREF(v);
+					Py_DECREF(w);
+					return x;
+				}
+			}
+			/* CoerceEx incremented the reference counts */
+			Py_DECREF(v);
+			Py_DECREF(w);
+		}
+	}
+	Py_INCREF(Py_NotImplemented);
+	return Py_NotImplemented;
+}
+
+static PyObject *
+binop_type_error(PyObject *v, PyObject *w, const char *op_name)
+{
+	/* ERROR */
+	print ("binop_type_error");
+	return NULL;
+}
+
+static PyObject *
+binary_op(PyObject *v, PyObject *w, const int op_slot, const char *op_name)
+{
+	PyObject *result = binary_op1(v, w, op_slot);
+	if (result == Py_NotImplemented) {
+		Py_DECREF(result);
+		return binop_type_error(v, w, op_name);
+	}
+	return result;
+}
+
+PyObject *
+PyNumber_Remainder(PyObject *v, PyObject *w)
+{
+	return binary_op(v, w, NB_SLOT(nb_remainder), "%");
+}
+
+PyObject *
+PyObject_Call(PyObject *func, PyObject *arg, PyObject *kw)
+{
+        ternaryfunc call;
+
+	if ((call = func->ob_type->tp_call) != NULL) {
+		PyObject *result = (*call)(func, arg, kw);
+		return result;
+	}
+	return NULL;
+}
