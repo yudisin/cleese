fc0192df6397b2af7562623f0e41a2a3a6102dc1
jtauber <jtauber@48ad7985-ff29-0410-b195-4b37543e66af>
2003-09-22 22:04:04 +0000

    implemented support for ANDing ints

--- trunk/experimental/echo/python/Objects/intobject.c
+++ trunk/experimental/echo/python/Objects/intobject.c
@@ -67,6 +67,24 @@ PyInt_Fini(void)
 
 extern int print_hex(long);
 
+#define CONVERT_TO_LONG(obj, lng)		\
+	if (PyInt_Check(obj)) {			\
+		lng = PyInt_AS_LONG(obj);	\
+	}					\
+	else {					\
+		Py_INCREF(Py_NotImplemented);	\
+		return Py_NotImplemented;	\
+	}
+
+static PyObject *
+int_and(PyIntObject *v, PyIntObject *w)
+{
+	register long a, b;
+	CONVERT_TO_LONG(v, a);
+	CONVERT_TO_LONG(w, b);
+	return PyInt_FromLong(a & b);
+}
+
 static int
 int_print(PyIntObject *v)
 {
@@ -74,8 +92,49 @@ int_print(PyIntObject *v)
 	return 0;
 }
 
+static PyNumberMethods int_as_number = {
+	0, //(binaryfunc)int_add,	/*nb_add*/
+	0, //(binaryfunc)int_sub,	/*nb_subtract*/
+	0, //(binaryfunc)int_mul,	/*nb_multiply*/
+	0, //(binaryfunc)int_classic_div, /*nb_divide*/
+	0, //(binaryfunc)int_mod,	/*nb_remainder*/
+	0, //(binaryfunc)int_divmod,	/*nb_divmod*/
+	0, //(ternaryfunc)int_pow,	/*nb_power*/
+	0, //(unaryfunc)int_neg,	/*nb_negative*/
+	0, //(unaryfunc)int_pos,	/*nb_positive*/
+	0, //(unaryfunc)int_abs,	/*nb_absolute*/
+	0, //(inquiry)int_nonzero,	/*nb_nonzero*/
+	0, //(unaryfunc)int_invert,	/*nb_invert*/
+	0, //(binaryfunc)int_lshift,	/*nb_lshift*/
+	0, //(binaryfunc)int_rshift,	/*nb_rshift*/
+	(binaryfunc)int_and,	/*nb_and*/
+	0, //(binaryfunc)int_xor,	/*nb_xor*/
+	0, //(binaryfunc)int_or,	/*nb_or*/
+	0, //int_coerce,		/*nb_coerce*/
+	0, //(unaryfunc)int_int,	/*nb_int*/
+	0, //(unaryfunc)int_long,	/*nb_long*/
+	0, //(unaryfunc)int_float,	/*nb_float*/
+	0, //(unaryfunc)int_oct,	/*nb_oct*/
+	0, //(unaryfunc)int_hex, 	/*nb_hex*/
+	0,			/*nb_inplace_add*/
+	0,			/*nb_inplace_subtract*/
+	0,			/*nb_inplace_multiply*/
+	0,			/*nb_inplace_divide*/
+	0,			/*nb_inplace_remainder*/
+	0,			/*nb_inplace_power*/
+	0,			/*nb_inplace_lshift*/
+	0,			/*nb_inplace_rshift*/
+	0,			/*nb_inplace_and*/
+	0,			/*nb_inplace_xor*/
+	0,			/*nb_inplace_or*/
+	0, //(binaryfunc)int_div,	/* nb_floor_divide */
+	0, //int_true_divide,	/* nb_true_divide */
+	0,			/* nb_inplace_floor_divide */
+	0,			/* nb_inplace_true_divide */
+};
+
 PyTypeObject PyInt_Type = {
-  PyObject_HEAD_INIT(&PyType_Type)
+	PyObject_HEAD_INIT(&PyType_Type)
 	0,
 	"int",
 	sizeof(PyIntObject),
@@ -86,16 +145,16 @@ PyTypeObject PyInt_Type = {
 	0,					/* tp_setattr */
 	0, //(cmpfunc)int_compare,			/* tp_compare */
 	0, //(reprfunc)int_repr,			/* tp_repr */
-	0, //&int_as_number,				/* tp_as_number */
+	&int_as_number,				/* tp_as_number */
 	0,					/* tp_as_sequence */
 	0,					/* tp_as_mapping */
 	0, //(hashfunc)int_hash,			/* tp_hash */
 	0,					/* tp_call */
-    0, //    (reprfunc)int_repr,			/* tp_str */
+	0, //    (reprfunc)int_repr,			/* tp_str */
 	0, //PyObject_GenericGetAttr,		/* tp_getattro */
 	0,					/* tp_setattro */
 	0,					/* tp_as_buffer */
-	0, //Py_TPFLAGS_DEFAULT | Py_TPFLAGS_CHECKTYPES | Py_TPFLAGS_BASETYPE,		/* tp_flags */
+	Py_TPFLAGS_DEFAULT | Py_TPFLAGS_CHECKTYPES | Py_TPFLAGS_BASETYPE,		/* tp_flags */
 	0, //int_doc,				/* tp_doc */
 	0,					/* tp_traverse */
 	0,					/* tp_clear */
