e64ac03e06b3880b1b1dd3821739ff005d3c832b
jtauber <jtauber@48ad7985-ff29-0410-b195-4b37543e66af>
2003-09-22 21:56:26 +0000

    added PyNumber_And

--- experimental/echo/python/Include/abstract.h
+++ experimental/echo/python/Include/abstract.h
@@ -7,8 +7,14 @@
 	 Returns the remainder of dividing o1 by o2, or null on
 	 failure.  This is the equivalent of the Python expression:
 	 o1%o2.
+       */
 
+     PyAPI_FUNC(PyObject *) PyNumber_And(PyObject *o1, PyObject *o2);
 
+       /*
+	 Returns the result of bitwise and of o1 and o2 on success, or
+	 NULL on failure. This is the equivalent of the Python
+	 expression: o1&o2.
        */
 
      PyAPI_FUNC(PyObject *) PyObject_GetItem(PyObject *o, PyObject *key);
@@ -17,7 +23,6 @@
 	 Return element of o corresponding to the object, key, or NULL
 	 on failure. This is the equivalent of the Python expression:
 	 o[key].
-
        */
 
      PyAPI_FUNC(PyObject *) PyObject_Call(PyObject *callable_object,
@@ -27,7 +32,6 @@
 	 Call a callable Python object, callable_object, with
 	 arguments and keywords arguments.  The 'args' argument can not be
 	 NULL, but the 'kw' argument can be NULL.
-
        */
 
 #endif /* Py_ABSTRACTOBJECT_H */
