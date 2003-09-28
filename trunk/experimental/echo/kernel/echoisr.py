a240fbec280c09b38ef8ec0c64d940eb1e1c0efa
dzl <dzl@48ad7985-ff29-0410-b195-4b37543e66af>
2003-09-28 19:39:29 +0000

    spartan ISR support

---
+++ trunk/experimental/echo/kernel/echoisr.py
@@ -0,0 +1,30 @@
+def more_chars():
+    return (inb(0x64) & 0x01)
+
+def get_scancode():
+    while not more_chars():
+        pass
+    return inb(0x60)
+
+def translate_scancode(scancode):
+    if (scancode & 0x80): # high bit set (key release)
+        return None
+    return "?E1234567890-=BTqwertyuiop[]N^asdfghjkl;'`Z\\zxcvbnm,./SXXXXXXXXX"[scancode & 0x3F]
+
+tb = textbuffer()
+ticks = 0
+
+def kbd_isr():
+	while more_chars():
+	    ch = translate_scancode(get_scancode())
+	    if ch:
+		print ch
+		tb[0] = ch; tb[1] = '\015'
+
+def clk_isr():
+	tb[158] = '01234567'[ticks & 7]
+
+setvec()
+
+while 1:
+	ticks = ticks + 1
