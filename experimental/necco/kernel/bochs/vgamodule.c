6b82817319ceb2bd1a8ccd67c17f05b6ec4e8367
dzl <dzl@48ad7985-ff29-0410-b195-4b37543e66af>
2003-10-03 02:04:48 +0000

    nothing left in vgamodule.c but buffers

--- experimental/necco/kernel/bochs/vgamodule.c
+++ experimental/necco/kernel/bochs/vgamodule.c
@@ -1,36 +1,13 @@
 #include "Python.h"
 
-static unsigned char saved[4000];
-
-static PyObject *
-vga_savetext(PyObject *self, PyObject *args)
-{
-	memcpy(saved, (void *)0xb8000, 80*25*2);
-	Py_INCREF(Py_True);
-	return Py_True;
-}
-
-static PyObject *
-vga_restoretext(PyObject *self, PyObject *args)
-{
-	memcpy((void *)0xb8000, saved, 80*25*2);
-	Py_INCREF(Py_True);
-	return Py_True;
-}
-
-static PyMethodDef vga_methods[] = {
-	{"savetext",	vga_savetext,	METH_VARARGS, NULL/*doc*/},
-	{"restoretext",	vga_restoretext,METH_VARARGS, NULL/*doc*/},
-	{NULL,		NULL},
-};
-
+static unsigned char savebuffer[4000];
 extern unsigned char bootscreen[];
 
 PyObject *
 _VGA_Init(void)
 {
 	PyObject *mod, *dict;
-	mod = Py_InitModule4("vga", vga_methods,
+	mod = Py_InitModule4("vga", NULL,
 			     NULL/*doc*/, (PyObject *)NULL,
 			     PYTHON_API_VERSION);
 
@@ -44,6 +21,8 @@ _VGA_Init(void)
 		PyBuffer_FromReadWriteMemory((void *)0xa0000, 0x10000));
 	SETBUILTIN("textbuffer",
 		PyBuffer_FromReadWriteMemory((void *)0xb8000, 80*25*2));
+	SETBUILTIN("savebuffer",
+		PyBuffer_FromReadWriteMemory((void *)savebuffer, 80*25*2));
 	SETBUILTIN("splashscreen",
 		PyBuffer_FromMemory(bootscreen, 0x10000));
 
