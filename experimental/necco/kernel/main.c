c48b42a135fc198d35b14f29492ceca368a63b9b
dzl <dzl@48ad7985-ff29-0410-b195-4b37543e66af>
2003-10-04 15:17:55 +0000

    hook up PyImport_FrozenModules directly, instead of in main (minor) remove printf's from import.c

--- experimental/necco/kernel/main.c
+++ experimental/necco/kernel/main.c
@@ -1,22 +1,17 @@
 #include "Python.h"
 
-extern struct _frozen frozenModules[];
 extern void Cleese_Initialize(), Cleese_Finalize();
-
-void
-Py_Main()
-{
-	PyImport_FrozenModules = frozenModules;
-	Py_Initialize();
-	Cleese_Initialize();
-	PyImport_ImportFrozenModule("__main__");
-	Cleese_Finalize();
-	Py_Finalize();
-}
 
 int
 main(void)
 {
-        Py_Main();
+	Py_Initialize();
+	Cleese_Initialize();
+
+	PyImport_ImportFrozenModule("__main__");
+
+	Cleese_Finalize();
+	Py_Finalize();
+
 	return 0;
 }
