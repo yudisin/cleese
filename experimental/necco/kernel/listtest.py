bfbb320fd0f0765b576cdb3a554dd4e3f814ef8a
jtauber <jtauber@48ad7985-ff29-0410-b195-4b37543e66af>
2003-10-12 22:48:56 +0000

    updated listtest with more tests

--- experimental/necco/kernel/listtest.py
+++ experimental/necco/kernel/listtest.py
@@ -14,4 +14,22 @@ print m.index("a")
 
 print tuple(m[2:4])
 
+for x in m:
+    print x
+
+d = {1: 1, 2:2 }
+
+print d[1]
+
+e = {"1": "1", "2": "2"}
+
+print e["1"]
+
+if 1 in (1,2):
+    print "yes"
+
+print "a" == "b"
+
+print "a" == "a"
+
 
