ce22bf6a3d18d6f753d52dc4445b038be82ad60c
dzl <dzl@48ad7985-ff29-0410-b195-4b37543e66af>
2003-10-14 06:47:24 +0000

    take advantage of better VM coverage

--- trunk/experimental/necco/kernel/soko.py
+++ trunk/experimental/necco/kernel/soko.py
@@ -8,111 +8,77 @@ blit.fill(pyvga.framebuffer,320,0,0,320,200,0)
 ################################################################################
 import isr
 import py8042
+import keyb
 
 bufchar = None
 
 def kbd_isr():
-	global bufchar
 	ch = keyb.translate_scancode(py8042.get_scancode())
 	if ch:
+		global bufchar
 		bufchar = ch
-
-def poll_mouse_loop():
-	fl = py8042.poll_mouse()
-	if fl:
-		poll_mouse_loop()
 
 dir = None
 def clk_isr():
 	global bufchar
 	global dir
+
 	blit.fill(pyvga.framebuffer, 320, 312, 0, 8, 8, (isr.ticker & 15) + 16)
 
-	poll_mouse_loop()
-
-	dx = py8042.dx
-	dy = py8042.dy
-	if   dx > 10:  dir = 'l'
-	elif dy > 10:  dir = 'k'
-	elif dx < 0:   dir = 'h'
-	elif dy < 0:   dir = 'j'
-	elif dx == 0 and dy == 0 and dir:
-		bufchar = dir
-		dir = None
+	if py8042.more_squeaks():
+		dx = dy = 0
+		while py8042.more_squeaks():
+			_,dx,dy = py8042.get_squeak()
+		if   dx > 10:  dir = 'l'
+		elif dy > 10:  dir = 'k'
+		elif dx < -10: dir = 'h'
+		elif dy < -10: dir = 'j'
+	elif dir:
+		bufchar = dir; dir = None
 	
-	py8042.clear_mouse()
-
 isr.setvec(clk_isr, kbd_isr)
 
 ################################################################################
-import keyb
-import buf
 
-map = buf.bss(400)
+#--test map--
+#map = list('     #####               #   #               #   #             ###   ##            #      #          ### # ## #   ###### #   # ## #####  ..# #   .$          ..# ##### ### #@##  ..#     #     #########     #######')
 
-#mapi = '     #####               #   #               #   #             ###   ##            #      #          ### # ## #   ###### #   # ## #####  ..# #   .$          ..# ##### ### #@##  ..#     #     #########     #######                                                                                                                                                                        '
-mapi = '     #####               #   #               #$  #             ###  $##            #  $ $ #          ### # ## #   ###### #   # ## #####  ..# # $  $          ..# ##### ### #@##  ..#     #     #########     #######                                                                                                                                                                        '
-#mapi = '           #######             #  ...#         #####  ...#         #      . .#         #  ##  ...#         ## ##  ...#        ### ########        # $$$ ##        #####  $ $ #####   ##   #$ $   #   #   #@ $  $    $  $ #   ###### $$ $ #####        #      #            ########                                                                                                          '
+#--easier level--
+map = list('     #####               #   #               #$  #             ###  $##            #  $ $ #          ### # ## #   ###### #   # ## #####  ..# # $  $          ..# ##### ### #@##  ..#     #     #########     #######')
 
-map[:len(mapi)] = mapi
-
-def where():
-	i = 0
-	l = len(mapi)
-	while i < l:
-		ch = ord(map[i])
-		if ch == ord('@'):	return i
-		if ch == ord('&'):	return i
-		i = i + 1
-	return -1
-
-def tile(x, y, char):
-	x = x << 3
-	y = y << 3
-	o = ord(char)
-	if   o == ord(' '): v = 0
-	elif o == ord('#'): v = 31
-	elif o == ord('.'): v = 5
-	elif o == ord('*'): v = 2
-	elif o == ord('$'): v = 4
-	elif o == ord('@'): v = 1
-	elif o == ord('&'): v = 1
-	blit.fill(pyvga.framebuffer, 320, x, y, 8, 8, v)
+#--harder level--
+#map = list('           #######             #  ...#         #####  ...#         #      . .#         #  ##  ...#         ## ##  ...#        ### ########        # $$$ ##        #####  $ $ #####   ##   #$ $   #   #   #@ $  $    $  $ #   ###### $$ $ #####        #      #            ########')
 
 def disptile(off):
-	y = 0
-	n = off
-	while n > 20:
-		n = n + -20
-		y = y + 1
-	x = n
-	tile(x, y, map[off])
+	blit.fill(pyvga.framebuffer, 320,
+		(off % 20) << 3, (off / 20) << 3,	# x, y
+		8, 8,					# dx, dy
+		{ ' ': 0, '#': 31, '.': 5,		# color
+	          '*': 2, '$': 4,
+	          '@': 1, '&': 1 }[map[off]])
 
-def dump(verbose):
-	off = 0
-	y = 0
-	while off < len(mapi):
-		x = 0
-		while x < 20:
-			tile(x, y, map[off])
-			off = off + 1
-			x = x + 1
-		y = y + 1
-
-def do_paint():
-	dump(0)
-
-s = buf.bss(3)
+def dispall():
+	i = len(map)
+	while i > 0:		# no for yet?
+		i = i - 1
+		disptile(i)
 
 def move(dir):
-	global s
-	soko = where()
+	if map.count('@'):	soko = map.index('@')
+	else:			soko = map.index('&')
 
+	s = list('~~~')
 	s[0] = map[soko]
 	s[1] = map[soko+dir]
 	s[2] = map[soko+dir+dir]
 
-	rewrite()
+	if s[1] in ' .':
+		s[0] = leave(s[0])
+		s[1] = enter(s[1])
+	elif s[1] in '$*' and s[2] in ' .':
+		s[0] = leave(s[0])
+		s[1] = enter(s[1])
+		s[2] = slide(s[2])
 
 	map[soko]         = s[0]
 	map[soko+dir]     = s[1]
@@ -122,61 +88,36 @@ def move(dir):
 	disptile(soko+dir)
 	disptile(soko+dir+dir)
 
-def rewrite():
-	s1 = ord(s[1])
-	if s1 == ord(' ') or s1 == ord('.'):
-		s[0] = leave(s[0])
-		s[1] = enter(s[1])
-	elif s1 == ord('$') or s1 == ord('*'):
-		s2 = ord(s[2])
-		if s2 == ord(' ') or s2 == ord('.'):
-			s[0] = leave(s[0])
-			s[1] = enter(s[1])
-			s[2] = slide(s[2])
-
 def leave(c):
-        if ord(c) == ord('@'): return ' '
-        else:        return '.'
+	if c == '@':	return ' '
+        else:        	return '.'
 
 def enter(c):
-        if ord(c) == ord(' ') or ord(c) == ord('$'): return '@'
-        else:                    return '&'
+	if c in ' $':	return '@'
+	else:		return '&'
 
 def slide(c):
-        if ord(c) == ord(' '): return '$'
-        else:        return '*'
+	if c == ' ':	return '$'
+        else:        	return '*'
 
-def stones():
-	i = 0
-	l = len(mapi)
-	while i < l:
-		if ord(map[i]) == ord('$'):
-			return 1
-		i = i + 1
-	return 0
+dispall()
+while 1:
+	def loop(msg):
+		pyvga.cleartext()
+		pyvga.entertext()
+		print msg
+		while 1: pass
 
-def loop(msg):
-	pyvga.cleartext()
-	pyvga.entertext()
-	print msg
-	while 1: pass
-
-do_paint()
-done = 0
-while not done:
-	if not stones():
+	if not map.count('$'):
 		loop('You Win!')
 
+	bufchar = None
 	while not bufchar:
 		pass
-	key = ord(bufchar)
-	bufchar = None
 
-	if key == ord('q'):	done = 1
-	elif key == ord('h'):	move(-1)
-	elif key == ord('j'):	move(20)
-	elif key == ord('k'):	move(-20)
-	elif key == ord('l'):	move(1)
-	elif key == ord('p'):	dump(1)
-
-loop('Quit')
+	if   bufchar == 'q':	loop('Thanks for playing')
+	elif bufchar == 'h':	move(-1)
+	elif bufchar == 'j':	move(20)
+	elif bufchar == 'k':	move(-20)
+	elif bufchar == 'l':	move(1)
+	elif bufchar == 'p':	dispall()
