b97d4ff9bf404f2e14caba7ff06056ee8a189f25
dzl <dzl@48ad7985-ff29-0410-b195-4b37543e66af>
2003-10-15 00:20:44 +0000

    stack swapping for coroutines

--- trunk/experimental/necco/kernel/stacktest.py
+++ trunk/experimental/necco/kernel/stacktest.py
@@ -1,13 +1,25 @@
 import buf
 import stack
 
-print '-- test stack swapping --'
+print '-- test stacks'
 
-def foo():
-	print 'in new stack'
-	return 'back on old'
+def recur(i):
+	if i:
+		print '-- swapped: %d' % i
+		recur(stack.swap(i - 1))
+	else:
+		stack.swap(None)
 
-newstack = buf.bss(1024)
-print stack.eval(newstack, foo)
+def worker():
+	recur(8)
+	print '** not reached **'
 
-print '-- test done --'
+newstack = buf.bss(0x800)
+stack.init(newstack, worker)
+
+i = stack.swap(None, newstack)
+while i:
+	print '-- swapped back: %d' % i
+	i = stack.swap(i - 1, newstack)
+
+print '-- test stacks done'
