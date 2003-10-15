a71ebd5b12b082a563bfee73395ccfe29355af67
dzl <dzl@48ad7985-ff29-0410-b195-4b37543e66af>
2003-10-15 00:20:44 +0000

    stack swapping for coroutines

--- experimental/necco/kernel/stackmodule.c
+++ experimental/necco/kernel/stackmodule.c
@@ -10,20 +10,55 @@ typedef struct {
 
 static PyObject *d = NULL;
 
+static void init_dict() {
+	PyObject *s = PyThreadState_Get()->interp->modules;
+	PyObject *m = PyDict_GetItemString(s, "__main__");
+	d = PyModule_GetDict(m);
+}
+
+
+extern PyObject *swapstacks();
+extern int initstack();
+
+static void *save_area(PyObject *stack)
+{
+	static void *initspsav;
+	return (stack ? ((PyBufferObject *)stack)->b_ptr
+		      : (void *)&initspsav);
+}
+
 static PyObject *
-stack_eval(PyObject *self, PyObject *args)
+stack_swap(PyObject *self, PyObject *args)
+{
+	static PyObject *curstack;
+	PyObject *obuf, *oarg;
+	void *from, *to;
+
+	obuf = NULL;
+	if(!PyArg_UnpackTuple(args, "swap", 1, 2, &oarg, &obuf))
+		return NULL;
+
+	if(!PyBuffer_Check(obuf) && obuf)
+		return NULL;
+
+	from = save_area(curstack);
+	to   = save_area(obuf);
+	curstack = obuf;
+
+	return swapstacks(from, to, oarg);
+}
+
+
+static PyObject *
+stack_init(PyObject *self, PyObject *args)
 {
 	PyObject *obuf, *ofunc;
 	PyBufferObject *pbo;
+	extern PyObject *PyEval_EvalCode();
 
-	if(!d)	{
-		PyObject *s = PyThreadState_Get()->interp->modules;
-		PyObject *m = PyDict_GetItemString(s, "__main__");
+	if(!d)	{ init_dict(); }
 
-		d = PyModule_GetDict(m);
-	}
-
-	if(!PyArg_UnpackTuple(args, "eval", 2, 2, &obuf, &ofunc))
+	if(!PyArg_UnpackTuple(args, "init", 2, 2, &obuf, &ofunc))
 		return NULL;
 
 	if(!PyBuffer_Check(obuf))
@@ -33,20 +68,23 @@ stack_eval(PyObject *self, PyObject *args)
 	if(pbo->b_readonly)
 		return NULL;
 
-	return swapandeval(pbo->b_ptr + pbo->b_size,
-				PyFunction_GET_CODE(ofunc), d);
+	*(int *)save_area(obuf) = initstack(
+			pbo->b_ptr + pbo->b_size,
+			PyFunction_GET_CODE(ofunc), d);
+
+	Py_INCREF(Py_True);
+	return Py_True;
 }
 
 static PyMethodDef stack_methods[] = {
-	{"eval",	stack_eval,     METH_VARARGS, NULL/*doc*/},
+	{"swap",	stack_swap,     METH_VARARGS, NULL/*doc*/},
+	{"init",	stack_init,	METH_VARARGS, NULL/*doc*/},
 	{NULL,		NULL},
 };
 
 PyObject *
 _Stack_Init(void)
 {
-	PyObject *mod, *dict;
-
 	return Py_InitModule4("stack", stack_methods,
 			     NULL/*doc*/, (PyObject *)NULL,
 			     PYTHON_API_VERSION);
